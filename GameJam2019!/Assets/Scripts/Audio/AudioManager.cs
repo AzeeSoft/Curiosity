ff760cd3c09255c4cca9a2713581573d0f331a0f
brandon <de>
2019-01-26 01:15:04 -0600

    adds audio manager

---
+++ GameJam2019!/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    //to play audio from the manager!!
+    //FindObjectOfType<AudioManager>().Play("");
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach(Sound s in sounds)
+        {
+            s.source =gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+ 
+            return;
+        }
+        s.source.Play();    
+        
+    }
+
+    void Start()
+    {
+        //Play Main theme song here 
+    }
+}

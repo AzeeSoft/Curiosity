c9370bf4a316b29c2d4eae5aa309cac098303640
TomasAbrams <TSAbrams3039@muleriders.saumag.edu>
2019-01-26 10:34:51 -0600

    Starts on animation system

---
+++ GameJam2019!/Assets/Scripts/HumanCutscenes/TerminalTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalTrigger : MonoBehaviour
+{
+   public enum State
+    {
+        First,
+        Second,
+        Last
+    }
+
+    public bool playingAnim;
+
+    public State currentAnim;
+
+    public GameObject poleLight;
+
+    public GameObject spawnPos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            switch (currentAnim)
+            {
+                case State.First:
+                    break;
+                case State.Second:
+                    break;
+                case State.Last:
+                    break;
+            }
+        }
+    }
+    
+}

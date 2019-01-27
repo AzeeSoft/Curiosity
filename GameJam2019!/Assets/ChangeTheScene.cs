a85ae6017856d42b877f0516350d80fd7b58d843
TomasAbrams <TSAbrams3039@muleriders.saumag.edu>
2019-01-27 15:58:37 -0600

    Credits scene

---
+++ GameJam2019!/Assets/ChangeTheScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeTheScene : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine("LoadScene");
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(10f);
+        SceneManager.LoadScene("MainMenu");
+    }
+}
--- GameJam2019!/Assets/Scripts/UI/Scroll.cs
+++ GameJam2019!/Assets/Scripts/UI/Scroll.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Scroll : MonoBehaviour
 {
     private RectTransform myRectTransform;
+    public float maxHeight;
     // Start is called before the first frame update
+
     void Start()
     {
        myRectTransform = GetComponent<RectTransform>();
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        myRectTransform.localPosition += Vector3.up;
+        myRectTransform.localPosition += (Vector3.up * Time.deltaTime) * 20;
+
+        if(myRectTransform.localPosition.y >= maxHeight)
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
     }
 }

8afd90f6b61901684cfd0ff9dadc32eede4233f8
Azee <aziztitu1996@gmail.com>
2019-01-27 07:29:03 -0600

    Updates Day&Night bounds.

--- GameJam2019!/Assets/Scripts/Sun.cs
+++ GameJam2019!/Assets/Scripts/Sun.cs
@@ -59,7 +59,7 @@ public class Sun : MonoBehaviour
         
         _hourOfDay = GetHourOfTheDay();
 
-        if (_hourOfDay > 2 && _hourOfDay < 12)
+        if (_hourOfDay >= 2 && _hourOfDay < 12)
         {
             _sunState = SunState.Day;
         }

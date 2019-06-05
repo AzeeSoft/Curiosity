ce7b0238773c897589b90d9dc98ad76d14263c69
TomasAbrams <TSAbrams3039@muleriders.saumag.edu>
2019-06-05 06:18:00 -0500

    Starts working on chasm respawn

---
+++ Curiosity/Assets/Chasm.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chasm : MonoBehaviour
+{
+    public GameObject spawn;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            LevelManager.Instance.CuriosityModel.gameObject.transform.localPosition = spawn.transform.position;
+        }
+    }
+}

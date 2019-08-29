a175c45429228fdb3152a9bfd2ba3d437a42d296
TomasAbrams <TSAbrams3039@muleriders.saumag.edu>
2019-08-29 06:07:39 -0500

    Adds intro tutorial

---
+++ Curiosity/Assets/Tutorial.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Tutorial : MonoBehaviour
+{
+    public PlayableAsset intro2;
+
+    public PlayableDirector dir1, dir2;
+
+    private bool intro1done = false;
+
+    private void Awake()
+    {
+        intro1done = true;
+    }
+
+    private void Update()
+    {
+        
+        if(intro1done)
+        {
+            if (Input.GetButtonUp("Scan"))
+            {
+                Debug.Log("starting part 2");
+                Destroy(dir1);
+                dir2.gameObject.SetActive(true);
+            }
+            
+        }
+    }
+}

7eb9fff30f75ae3aa710523942475c2522b3d5ed
TomasAbrams <TSAbrams3039@muleriders.saumag.edu>
2019-09-01 04:22:04 -0500

    Env Changes, main menu changes

---
+++ Curiosity/Assets/MainMenuManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject main, settings;
+
+    public enum State
+    {
+        Main,
+        Settings
+    }
+
+    public State currentState;
+
+    public void Update()
+    {
+        switch (currentState)
+        {
+            case State.Main:
+                settings.SetActive(false);
+                main.SetActive(true);
+                break;
+            case State.Settings:
+                settings.SetActive(true);
+                main.SetActive(false);
+                break;
+        }
+    }
+
+    public void SwitchState(int state)
+    {
+        if(state == 0)
+        {
+            currentState = State.Main;
+        }
+        else
+        {
+            currentState = State.Settings;
+        }
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}

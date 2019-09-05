cd8ad82d4da635c8bc51290fa147c8c5f8fbd0d8
TomasAbrams <TSAbrams3039@muleriders.saumag.edu>
2019-09-05 15:31:47 -0500

    Thrusters Are Now Unlockable

---
+++ Curiosity/Assets/ThrusterUnlock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrusterUnlock : MonoBehaviour
+{
+    public Color startingColor;
+    private AudioSource source;
+    public Light light;
+
+    private void Start()
+    {
+        this.GetComponentInChildren<MeshRenderer>().sharedMaterial.SetColor("_EmissionColor", startingColor * 4);
+        source = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if(other.GetComponentInParent<ThrusterController>().enabled == false)
+            {
+                other.GetComponentInParent<ThrusterController>().enabled = true;
+
+                this.GetComponentInChildren<MeshRenderer>().sharedMaterial.SetColor("_EmissionColor", Color.black);
+                source.Play();
+                light.enabled = false;
+            }
+           
+        }
+    }
+}

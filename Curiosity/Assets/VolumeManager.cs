af5b482e1f0ffd1ce04cebcedd4f0af046c7f163
TomasAbrams <TSAbrams3039@muleriders.saumag.edu>
2019-09-10 06:06:06 -0500

    Adds player prefs to volume manager

--- Curiosity/Assets/VolumeManager.cs
+++ Curiosity/Assets/VolumeManager.cs
@@ -12,32 +12,36 @@ public class VolumeManager : MonoBehaviour
 
     private void Start()
     {
-        float currentMasterVol;
-        masterVol.audioMixer.GetFloat("MasterVol", out currentMasterVol);
-        masterSlide.value = Mathf.Pow(10.0f, currentMasterVol / 20.0f);
+        masterSlide.value = PlayerPrefs.GetFloat("MasterVol", 1);
+        masterVol.audioMixer.SetFloat("MasterVol", 20.0f * Mathf.Log10(masterSlide.value));
 
-        float currentMusicVol;
-        Music.audioMixer.GetFloat("MusicVol", out currentMusicVol);
-        MusicSlide.value = Mathf.Pow(10.0f, currentMusicVol / 20.0f);
+        MusicSlide.value = PlayerPrefs.GetFloat("MusicVol", 1);
+        Music.audioMixer.SetFloat("MusicVol", 20.0f * Mathf.Log10(MusicSlide.value));
 
-        float currentSFXVol;
-        SFX.audioMixer.GetFloat("SFXVol", out currentSFXVol);
-        SFXSlide.value = Mathf.Pow(10.0f, currentSFXVol / 20.0f);
+        SFXSlide.value = PlayerPrefs.GetFloat("SFXVol", 1);
+        SFX.audioMixer.SetFloat("SFXVol", 20.0f * Mathf.Log10(SFXSlide.value));
     }
 
     public void setMasterVolume(float _value)
     {
         masterVol.audioMixer.SetFloat("MasterVol", 20.0f * Mathf.Log10(_value));
         Debug.Log(_value);
+        PlayerPrefs.SetFloat("MasterVol", _value);
+        PlayerPrefs.Save(); 
     }
     public void setSFXVolume(float _value)
     {
         SFX.audioMixer.SetFloat("SFXVol", 20.0f * Mathf.Log10(_value));
+        PlayerPrefs.SetFloat("SFXVol", _value);
+        PlayerPrefs.Save();
         Debug.Log(_value);
     }
     public void setMusicVolume(float _value)
     {
         Music.audioMixer.SetFloat("MusicVol", 20.0f * Mathf.Log10(_value));
+        PlayerPrefs.SetFloat("MusicVol", _value);
+        PlayerPrefs.Save();
+        Debug.Log("saved music volume. " + PlayerPrefs.GetFloat("MusicVol", 0));
         Debug.Log(_value);
     }
 }

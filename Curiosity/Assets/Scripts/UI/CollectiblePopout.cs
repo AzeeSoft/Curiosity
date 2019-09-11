b701adb2d666b6ec4dd5af66063a7fe886df7b55
Azee <aziztitu1996@gmail.com>
2019-09-11 04:07:28 -0500

    Implements collectible popout.

---
+++ Curiosity/Assets/Scripts/UI/CollectiblePopout.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectiblePopout : MonoBehaviour
+{
+    public GameObject collectibleCounter;
+    public float showDuration = 3f;
+
+    private Coroutine _showAndHideCoroutine = null;
+
+    void Awake()
+    {
+        collectibleCounter.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CollectibleManager.Instance.onNewCollectibleCollected += OnNewCollectibleCollected;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnNewCollectibleCollected(ResearchScannable researchScannable)
+    {
+        if (_showAndHideCoroutine != null)
+        {
+            StopCoroutine(_showAndHideCoroutine);
+        }
+
+        _showAndHideCoroutine = StartCoroutine(ShowAndHide());
+    }
+
+    IEnumerator ShowAndHide()
+    {
+        collectibleCounter.SetActive(true);
+        yield return new WaitForSecondsRealtime(showDuration);
+        collectibleCounter.SetActive(false);
+
+        _showAndHideCoroutine = null;
+    }
+}
